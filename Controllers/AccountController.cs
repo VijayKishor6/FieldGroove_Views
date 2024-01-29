f68f9c1b60a5adeec471252a48839e38997c67cc
VIJAY KISHOR G P <88893339+VijayKishor6@users.noreply.github.com>
2024-01-29 10:43:34 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FieldGroove_Views.Controllers
+{
+    public class AccountController : Controller
+    {
+       public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+    
+    }
+}
---
+++ Controllers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FieldGroove_Views.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
