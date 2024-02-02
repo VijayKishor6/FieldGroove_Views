e387d4076be0d100c29a0b53a80e32569e490e7c
VIJAY KISHOR G P <88893339+VijayKishor6@users.noreply.github.com>
2024-02-02 18:35:21 +0530

    LeadsCreation UI

---
+++ Controllers/LeadsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FieldGroove_Views.Controllers
+{
+    public class LeadsController : Controller
+    {
+        public IActionResult LeadsCreation()
+        {
+            return View();
+        }
+    }
+}   
