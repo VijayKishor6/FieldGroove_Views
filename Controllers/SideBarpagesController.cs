a2fd57471cd9d1072e894c3b53786090e6680256
Kavinkumars23 <141397995+Kavinkumars23@users.noreply.github.com>
2024-02-08 19:31:08 +0530

    Created Settings

---
+++ Controllers/SideBarpagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FieldGroove_Views.Controllers
+{
+    public class SideBarpagesController : Controller
+    {
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
